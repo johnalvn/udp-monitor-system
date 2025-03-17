921af1fb1ff4eb0cdbbaec814c67ffe37b1210e5
John Alvarez <jean1052@gmail.com>
2025-03-16 22:01:40 -0500

    Start monitoring service

--- monitoring-service/build.gradle.kts
+++ monitoring-service/build.gradle.kts
@@ -11,6 +11,14 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("com.rabbitmq:amqp-client:5.20.0")
+    implementation("com.google.code.gson:gson:2.8.9")
+    implementation("org.yaml:snakeyaml:2.0")
+    // https://mvnrepository.com/artifact/io.mockk/mockk
+    testImplementation("io.mockk:mockk:1.13.10")
+    testImplementation("org.mockito:mockito-core:5.14.2")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.14.2")
+
 }
 
 tasks.test {
---
+++ monitoring-service/src/main/kotlin/integration/rabbitmq/Consumer.kt
@@ -0,0 +1,39 @@
+package co.jedal.test.integration.rabbitmq
+
+import co.jedal.test.integration.handler.SensorRawMessageHandler
+import com.rabbitmq.client.ConnectionFactory
+import com.rabbitmq.client.DeliverCallback
+import java.nio.charset.StandardCharsets
+
+class Consumer {
+
+    fun consumeFromExchange() {
+        val factory = ConnectionFactory().apply {
+            host = "localhost"
+            username = "user"
+            password = "password"
+        }
+
+        val connection = factory.newConnection()
+        val channel = connection.createChannel()
+
+        val exchangeName = "logs"
+        val exchangeType = "fanout"
+        channel.exchangeDeclare(exchangeName, exchangeType)
+
+        val queueName = channel.queueDeclare().queue
+
+        channel.queueBind(queueName, exchangeName, "")
+
+        println(" [*] Waiting for messages from exchange '$exchangeName'. To exit press CTRL+C")
+
+        val deliverCallback = DeliverCallback { _, delivery ->
+            val message = String(delivery.body, StandardCharsets.UTF_8)
+            println(" [x] Received '$message'")
+            SensorRawMessageHandler().handler(message)
+        }
+
+        // Start consuming (auto-ack = true)
+        channel.basicConsume(queueName, true, deliverCallback) { _ -> }
+    }
+}
